85c2ed0a36a86ebfc5933aa97bf2208e5780cd4e
Alexander Schellenberg <alexander.schellenberg@mindcurv.com>
2016-06-09 10:28:05 +0200

    Added new formations and switching almost done, new enemy and new enemylasers

---
+++ Assets/Scripts/Formations.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Formations : MonoBehaviour {
+
+    public GameObject[] formations;
+    private bool newFormationSet = false;
+	
+    void Start()
+    {
+        formations[0] = Instantiate(formations[0], formations[0].transform.position, Quaternion.identity) as GameObject;
+        formations[0].transform.parent = this.transform;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	    if (EnemySpawner.enemyWave == 2)
+        {
+            Debug.Log(EnemySpawner.enemyWave);
+            if (!newFormationSet)
+            {
+                formations[0].SetActive(false);
+                formations[1] = Instantiate(formations[1], formations[1].transform.position, Quaternion.identity) as GameObject;
+                formations[1].transform.parent = this.transform;
+                Debug.Log("Formation set : " + newFormationSet);
+                newFormationSet = true;
+            }
+        }
+	}
+
+    void EnableFormation(int formationPos)
+    {
+        Debug.Log(formationPos + " Name: " + formations[formationPos].name);
+        formations[formationPos].SetActive(true);
+    }
+}
